80c241fccda7837d61ead38a4e9a34983dace674
Eitan Lees <eitanlees@gmail.com>
2025-02-12 10:15:16 -0500

    Trying to clean up a few things

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://eitanlees.github.io',
                 'git_url': 'https://github.com/eitanlees/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.mymod': { 'nbdev_hello_world.mymod.foo': ('mymod.html#foo', 'nbdev_hello_world/mymod.py'),
+                                         'nbdev_hello_world.mymod.say_hello': ('mymod.html#say_hello', 'nbdev_hello_world/mymod.py')}}}
